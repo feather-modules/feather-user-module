8c4892c385a162f4b1b5a929b112b2a902a63713
GErP83 <gurrka@gmail.com>
2024-08-20 20:02:49 +0200

    Update Auth+Interface.swift

--- Sources/UserModuleKit/Interfaces/Auth+Interface.swift
+++ Sources/UserModuleKit/Interfaces/Auth+Interface.swift
@@ -12,6 +12,10 @@ public protocol UserAuthInterface: Sendable {
     func auth(
         _ token: String
     ) async throws -> User.Auth.Response
+    
+    func auth(
+        id: ID<User.Account>
+    ) async throws -> User.Auth.Response
 
     func auth(
         _ credentials: User.Auth.Request
