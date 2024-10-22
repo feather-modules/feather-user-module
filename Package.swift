6aa99a7bf7bc3841aa3bb624b306ca89b090db3a
GErP83 <gurrka@gmail.com>
2024-10-22 12:21:30 +0200

    add swiftformatignore

--- Package.swift
+++ Package.swift
@@ -13,86 +13,39 @@ let package = Package(
     products: [
         .library(name: "UserModuleKit", targets: ["UserModuleKit"]),
         .library(name: "UserModule", targets: ["UserModule"]),
-        .library(
-            name: "UserModuleMigrationKit",
-            targets: ["UserModuleMigrationKit"]
-        ),
-        .library(
-            name: "UserModuleDatabaseKit",
-            targets: ["UserModuleDatabaseKit"]
-        ),
+        .library(name: "UserModuleMigrationKit", targets: ["UserModuleMigrationKit"]),
+        .library(name: "UserModuleDatabaseKit", targets: ["UserModuleDatabaseKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio", from: "2.61.0"),
-        .package(
-            url: "https://github.com/binarybirds/swift-bcrypt",
-            from: "1.0.2"
-        ),
-        .package(
-            url:
-                "https://github.com/feather-framework/feather-mail-driver-memory",
-            .upToNextMinor(from: "0.3.0")
-        ),
-        .package(
-            url:
-                "https://github.com/feather-framework/feather-push-driver-memory",
-            .upToNextMinor(from: "0.4.0")
-        ),
-        .package(
-            url:
-                "https://github.com/feather-framework/feather-database-driver-sqlite",
-            .upToNextMinor(from: "0.4.0")
-        ),
-        .package(
-            url: "https://github.com/feather-framework/feather-module-kit",
-            .upToNextMinor(from: "0.5.0")
-        ),
-        .package(
-            url: "https://github.com/feather-modules/feather-system-module",
-            .upToNextMinor(from: "0.17.0")
-        ),
-        .package(
-            url: "https://github.com/feather-framework/feather-validation",
-            .upToNextMinor(from: "0.1.1")
-        ),
-        .package(
-            url: "https://github.com/feather-framework/feather-access-control",
-            .upToNextMinor(from: "0.2.0")
-        ),
+        .package(url: "https://github.com/binarybirds/swift-bcrypt", from: "1.0.2"),
+        .package(url: "https://github.com/feather-framework/feather-mail-driver-memory", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/feather-framework/feather-push-driver-memory", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/feather-framework/feather-database-driver-sqlite", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/feather-framework/feather-module-kit", .upToNextMinor(from: "0.5.0")),
+        .package(url: "https://github.com/feather-modules/feather-system-module", .upToNextMinor(from: "0.17.0")),
+        .package(url: "https://github.com/feather-framework/feather-validation", .upToNextMinor(from: "0.1.1")),
+        .package(url: "https://github.com/feather-framework/feather-access-control", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         .target(
             name: "UserModuleKit",
             dependencies: [
-                .product(
-                    name: "FeatherModuleKit",
-                    package: "feather-module-kit"
-                ),
-                .product(
-                    name: "SystemModuleKit",
-                    package: "feather-system-module"
-                ),
+                .product(name: "FeatherModuleKit", package: "feather-module-kit"),
+                .product(name: "SystemModuleKit", package: "feather-system-module"),
                 .product(name: "FeatherACL", package: "feather-access-control"),
             ]
         ),
         .target(
             name: "UserModuleDatabaseKit",
-            dependencies: [
-                .target(name: "UserModuleKit")
-            ]
+            dependencies: [.target(name: "UserModuleKit")]
         ),
         .target(
             name: "UserModule",
             dependencies: [
                 .product(name: "Bcrypt", package: "swift-bcrypt"),
-                .product(
-                    name: "SystemModule",
-                    package: "feather-system-module"
-                ),
-                .product(
-                    name: "FeatherValidationFoundation",
-                    package: "feather-validation"
-                ),
+                .product(name: "SystemModule", package: "feather-system-module"),
+                .product(name: "FeatherValidationFoundation", package: "feather-validation"),
                 .target(name: "UserModuleDatabaseKit"),
             ]
         ),
@@ -101,10 +54,7 @@ let package = Package(
             name: "UserModuleMigrationKit",
             dependencies: [
                 .product(name: "Bcrypt", package: "swift-bcrypt"),
-                .product(
-                    name: "SystemModuleMigrationKit",
-                    package: "feather-system-module"
-                ),
+                .product(name: "SystemModuleMigrationKit", package: "feather-system-module"),
                 .target(name: "UserModuleDatabaseKit"),
             ]
         ),
@@ -124,18 +74,9 @@ let package = Package(
                 .target(name: "UserModule"),
                 .target(name: "UserModuleMigrationKit"),
                 // drivers
-                .product(
-                    name: "FeatherMailDriverMemory",
-                    package: "feather-mail-driver-memory"
-                ),
-                .product(
-                    name: "FeatherPushDriverMemory",
-                    package: "feather-push-driver-memory"
-                ),
-                .product(
-                    name: "FeatherDatabaseDriverSQLite",
-                    package: "feather-database-driver-sqlite"
-                ),
+                .product(name: "FeatherMailDriverMemory", package: "feather-mail-driver-memory"),
+                .product(name: "FeatherPushDriverMemory", package: "feather-push-driver-memory"),
+                .product(name: "FeatherDatabaseDriverSQLite", package: "feather-database-driver-sqlite"),
             ]
         ),
     ]
